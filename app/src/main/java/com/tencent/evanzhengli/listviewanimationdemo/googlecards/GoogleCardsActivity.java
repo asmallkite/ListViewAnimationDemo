ae51c4ffbe59f8e41f0a65f9de806d0c91eb1fcf
Evan <evanzhengli@tencent.com>
2018-09-01 10:14:35 +0800

    add AppearanceActivity

---
+++ app/src/main/java/com/tencent/evanzhengli/listviewanimationdemo/googlecards/GoogleCardsActivity.java
@@ -0,0 +1,12 @@
+package com.tencent.evanzhengli.listviewanimationdemo.googlecards;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoogleCardsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
