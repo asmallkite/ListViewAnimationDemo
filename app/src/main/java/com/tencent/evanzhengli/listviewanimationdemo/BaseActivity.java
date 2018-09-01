b706b62d6ce5dacbdc3f53252f3a71c9da35f1f2
Evan <evanzhengli@tencent.com>
2018-09-01 10:09:34 +0800

    add AppearanceActivity

--- app/src/main/java/com/tencent/evanzhengli/listviewanimationdemo/BaseActivity.java
+++ app/src/main/java/com/tencent/evanzhengli/listviewanimationdemo/BaseActivity.java
@@ -1,9 +1,11 @@
 package com.tencent.evanzhengli.listviewanimationdemo;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 public class BaseActivity extends Activity {
 
@@ -11,6 +13,8 @@ public class BaseActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+
         if (getActionBar() != null) {
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
--- app/src/main/java/com/tencent/evanzhengli/listviewanimationdemo/MainActivity.java
+++ app/src/main/java/com/tencent/evanzhengli/listviewanimationdemo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.tencent.evanzhengli.listviewanimationdemo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void onAppearanceClicked(final View view) {
+        Intent intent = new Intent(this, AppearanceExamplesActivity.class);
+        startActivity(intent);
     }
 }
